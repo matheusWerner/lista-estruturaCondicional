edef54a71ce007126ab5d658ede23aec2a2023bf
Matheus Ruan Werner <matheus.lol45@gmail.com>
2018-04-23 11:27:00 -0300

    exemplo while

---
+++ exemploWhile.java
@@ -0,0 +1,34 @@
+import javax.swing.JOptionPane;
+
+public clss ExemploWhile{
+
+    public static void main (String[] args){
+
+        int quantidadeMaxima = 4;
+        int atual = 0;
+        int maiorIdade = Integer.MIN_VALUE;
+        int menorIdade = Integer.MAX_VALUE;
+        
+
+        while (atual < quantidaxeMaxima){
+            String nome = JOptionPane.showInputDialog(null,
+                              "Digite o se nome");
+
+            int idade = Integer.parseInt("Digite a sua idade"));
+
+            if (idade > maiorIdade){
+                maiorIdade = idade;
+            } 
+                
+
+            System.out.print("Atual: " + atul);
+            atual = atual +1;                  
+        }
+
+        JOptionPane.showMessageDialog(null, "A maior idade ´´e: " + maior Idade);
+
+
+           
+           
+    }
+}
